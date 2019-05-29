386905aec4c204848ccecfb30e1f62d305826d63
kydann <kydann.2907@gmail.com>
2019-05-28 19:06:15 -0500

    Archivos admin

---
+++ EurekaAdmin/src/main/java/com/spring/eurekaadmin/EurekaadminApplication.java
@@ -0,0 +1,18 @@
+package com.spring.eurekaadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@EnableAdminServer
+@SpringBootApplication
+public class EurekaadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaadminApplication.class, args);
+	}
+
+}
